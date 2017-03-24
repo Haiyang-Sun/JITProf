f4ea87c84e973ca5d3f9c5b027538d931c911800
haiyang <sunhaiyangsjtu@gmail.com>
2017-03-24 06:22:59 +0100

    updated scripts

--- script/jitprof-our.sh
+++ script/jitprof-our.sh
@@ -8,5 +8,5 @@ if [ -z ${GRAALNODE+x} ]; then
     echo "GRAALNODE is not set, use default node"
     GRAALNODE="node"
 fi
-NODE="${GRAALNODE} -J-Dtruffle.customizedJITProf=true -J-Dgraal.TraceTruffleCompilation=false -J-Dtruffle.new.profiling=true"$2
-$NODE ../octane/harness.js "$1"
+NODE="${GRAALNODE} -J-Dtruffle.customizedJITProf=true -J-Dgraal.TraceTruffleCompilation=false -J-Dtruffle.new.profiling=true"
+$NODE "$1"
--- script/jitprof-truffle.sh
+++ script/jitprof-truffle.sh
@@ -4,7 +4,10 @@
 
 # run JITProf with sampling
 echo "analyzing with JITProf..."
-GRAALNODE="/Users/haiyang/Documents/WorkSpace/Graal/graalvm-0.18-dk/bin/node"
+if [ -z ${GRAALNODE+x} ]; then
+    echo "GRAALNODE is not set, use default node"
+    GRAALNODE="node"
+fi
 NODE="${GRAALNODE} -J-Dgraal.TraceTruffleCompilation=false -J-Dtruffle.new.profiling=true"
 analyses=""
 while IFS='' read -r line || [[ -n "$line" ]]; do
